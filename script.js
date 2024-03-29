e62c646e4a005c774d3fc335137f2aaf1b84b45d
omrxedd <165358068+omrxedd@users.noreply.github.com>
2024-03-29 12:53:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.body.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)}`
+    function change(){
+        document.body.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)}`
+    }
